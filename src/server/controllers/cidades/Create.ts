69f2cf08aba5a48c855999e8ba006946dc79dd81
Ismael <ismael.henrique.dev@gmail.com>
2024-08-29 21:36:24 -0300

    Middleware de validação com Yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express"
+import { Request, RequestHandler, Response } from "express"
 import * as yup from "yup"
 
 interface Icidade {
@@ -9,14 +9,12 @@ const bodyValidation: yup.ObjectSchema<Icidade> = yup.object().shape({
   nome: yup.string().required().min(3),
 })
 
-//coloquei esses dois primeiros colchetes para eu conseguir acessar o terceiro parametro e passar a minha interface
-export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
-  let validateData: Icidade | undefined = undefined
-
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
   try {
-    validateData = await bodyValidation.validate(req.body, {
+    await bodyValidation.validate(req.body, {
       abortEarly: false, // esse método serve para validar todos os campos
     })
+    return next()
   } catch (err) {
     const yupError = err as yup.ValidationError
     const Erros: Record<string, string> = {}
@@ -28,9 +26,44 @@ export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
     })
 
     return res.status(400).json({
-      Erros
+      Erros,
     })
   }
+}
+
+interface IFilter {
+  filter?: string
+}
+
+const queryValidation: yup.ObjectSchema<IFilter> = yup.object().shape({
+  filter: yup.string().required().min(3),
+})
+
+export const createQueryValidator: RequestHandler = async (req, res, next) => {
+  try {
+    await queryValidation.validate(req.query, {
+      abortEarly: false, // esse método serve para validar todos os campos
+    })
+    return next()
+  } catch (err) {
+    const yupError = err as yup.ValidationError
+    const Erros: Record<string, string> = {}
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return
+
+      Erros[error.path] = error.message
+    })
+
+    return res.status(400).json({
+      Erros,
+    })
+  }
+}
+
+//coloquei esses dois primeiros colchetes para eu conseguir acessar o terceiro parametro e passar a minha interface
+export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
+  let validateData: Icidade | undefined = undefined
 
   console.log(validateData)
 
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express"
 import { cidadesController } from "../controllers"
-
-
+import { createQueryValidator } from "../controllers/cidades/Create"
 
 const router = Router()
 
@@ -9,9 +8,14 @@ router.get("/", (req, res) => {
   return res.send("rodando")
 })
 
-// essa arrow function indica que é um controller caso ela devolver uma resp para o usuário, ou 
+// essa arrow function indica que é um controller caso ela devolver uma resp para o usuário, ou
 // caso ela esteja manipulando os dados para inserir algo no database
 
-router.post("/cidades", cidadesController.create)
+router.post(
+  "/cidades",
+  cidadesController.createBodyValidator,
+  cidadesController.createQueryValidator,
+  cidadesController.create
+)
 
 export { router }
